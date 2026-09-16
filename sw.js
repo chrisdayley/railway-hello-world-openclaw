a7f6d5db9d3303e796d935f3ca152e716a5a379c
chrisdayley <chris@smart-cro.com>
2026-09-16 15:25:34 -0600

    Bump Everlight offline cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='everlight-v1-offline';
+const CACHE='everlight-v2-offline';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
