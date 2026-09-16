8cc573848a1fa56c6e855441f39b97a677140121
chrisdayley <chris@smart-cro.com>
2026-09-16 15:44:02 -0600

    Cache region data offline

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='everlight-v4-offline';
+const CACHE='everlight-v4-offline-b';
 const CORE=[
   './','./index.html','./manifest.webmanifest',
   './icons/icon-192.svg','./icons/icon-512.svg',
   './data/world-events.json','./data/properties.json','./data/loot-tables.json',
-  './data/quests.json','./data/factions.json','./data/companions.json','./data/crafting.json'
+  './data/quests.json','./data/factions.json','./data/companions.json','./data/crafting.json','./data/regions.json'
 ];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
