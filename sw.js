0582efc407451a91501023b7cedfca2eff30c36f
chrisdayley <chris@smart-cro.com>
2026-09-16 15:39:41 -0600

    Cache expanded game data for offline play

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE='everlight-v2-offline';
-const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE='everlight-v3-offline';
+const CORE=[
+  './','./index.html','./manifest.webmanifest',
+  './icons/icon-192.svg','./icons/icon-512.svg',
+  './data/world-events.json','./data/properties.json','./data/loot-tables.json'
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
